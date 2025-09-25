76374a2642dee6262b50142f35cfc18aea70cf15
Daniela Cardozo <mdanielacardozo@gmail.com>
2025-09-24 21:52:08 -0300

    add funcion crearContacto

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
 import Contacto from "./contacto.js";
 
-const btnAgregarContacto = document.getElementById('btnAgregarContacto');
+const btnAgregarContacto = document.getElementById("btnAgregarContacto");
 console.log(btnAgregarContacto);
+const formularioContacto = document.getElementById("formContacto");
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const inputTelefono = document.getElementById("telefono");
+const inputDireccion = document.getElementById("direccion");
+const inputNotas = document.getElementById("notas");
+const inputImagen = document.getElementById("imagen");
+const inputPuestoTrabajo = document.getElementById("puestoTrabajo");
+const inputEmpresa = document.getElementById("empresa");
 
-const modalFormularioContacto = new bootstrap.Modal(document.getElementById('contactoModal'));
+const crearContacto = () => {
+  const contactoNuevo = new Contacto(
+    inputNombre.value,
+    inputApellido.value,
+    inputTelefono.value,
+    inputEmail.value,
+    inputImagen.value.lenght !== 0
+      ? inputImagen.value
+      : "https://images.pexels.com/photos/28216688/pexels-photo-28216688.png",
+    inputEmpresa.value,
+    inputPuestoTrabajo.value,
+    inputDireccion.value,
+    inputNotas.value  
 
-btnAgregarContacto.addEventListener('click', () => {
-    modalFormularioContacto.show();
-})
+  );
+};
+
+const modalFormularioContacto = new bootstrap.Modal(
+  document.getElementById("contactoModal")
+);
+
+btnAgregarContacto.addEventListener("click", () => {
+  modalFormularioContacto.show();
+});
