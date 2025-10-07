26d11b7dc0807569e3480b9f1ff9fb4430eff4e9
Daniela Cardozo <mdanielacardozo@gmail.com>
2025-10-06 21:10:32 -0300

    add prepararContacto and editarContacto

--- js/app.js
+++ js/app.js
@@ -14,9 +14,12 @@ const inputPuestoTrabajo = document.getElementById("puestoTrabajo");
 const inputEmpresa = document.getElementById("empresa");
 const tbody = document.getElementById("tablaContactosBody");
 const tabla = document.querySelector(".table-responsive");
+const tituloModal = document.getElementById("contactoModalLabel");
 const seccionDetalles = document.getElementById("seccionDetalleContacto");
 const seccionTablaContactos = document.getElementById("seccionTablaContactos");
 
+
+let estoyCreando = true;
 let inputID = document.getElementById("contactoId");
 
 const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
@@ -28,7 +31,7 @@ const guardarLocalStorage = () => {
 
 const crearContacto = () => {
   const contactoNuevo = new Contacto(
-    inputID = null,
+    (inputID = null),
     inputNombre.value,
     inputApellido.value,
     inputTelefono.value,
@@ -83,7 +86,7 @@ const dibujarFila = (itemContacto, fila) => {
                 <td>${itemContacto.apellido}</td>
                 <td>${itemContacto.telefono}</td>
                 <td>
-                  <img src=${itemContacto.imagen} alt=${itemContacto.nombre} class="img-thumbnail img-table" w-100/>
+                  <img src=${itemContacto.imagen} alt=${itemContacto.nombre} class="img-thumbnail img-table" width="80px"/>
                 </td>
                 <td>
                   <button
@@ -112,70 +115,124 @@ const dibujarFila = (itemContacto, fila) => {
   `;
 };
 
-
 window.borrarContacto = (id) => {
   Swal.fire({
-  title: "Estas seguro de eliminar el contacto?",
-  text: "No podes revertir este paso",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Borrar",
-  cancelButtonText: "Cancelar",
-}).then((result) => {
-  console.log(result);
-  
-  if (result.isConfirmed) {
+    title: "Estas seguro de eliminar el contacto?",
+    text: "No podes revertir este paso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    console.log(result);
 
-    //bucar la posicion del elemento que quiero borrar
-    const indiceContacto = agenda.findIndex((contacto) => contacto.id === id);
-    //borrar un contacto del array agenda
-    agenda.splice(indiceContacto, 1);
+    if (result.isConfirmed) {
+      //bucar la posicion del elemento que quiero borrar
+      const indiceContacto = agenda.findIndex((contacto) => contacto.id === id);
+      //borrar un contacto del array agenda
+      agenda.splice(indiceContacto, 1);
 
-    guardarLocalStorage();
+      guardarLocalStorage();
 
-    tbody.children[indiceContacto].remove();
+      tbody.children[indiceContacto].remove();
 
-    if (tbody.children.length === 0) {
-      mostrarNoHayDisponibles();
-    }
-
-    const filasRestantes = tbody.children;
-
-    for (let i = 0; i < filasRestantes.length; i++) {
-      const celdaIndice = filasRestantes[i].querySelector('th');
-      if (celdaIndice) {
-        celdaIndice.textContent = i + 1;
+      if (tbody.children.length === 0) {
+        mostrarNoHayDisponibles();
       }
-      
-    }
 
-    Swal.fire({
-      title: "Contacto eliminado",
-      text: "El contacto fue eliminado satisfactoriamente",
-      icon: "success"
-    });
-    console.log(agenda);
-    
-  }
-});
-}
+      const filasRestantes = tbody.children;
+
+      for (let i = 0; i < filasRestantes.length; i++) {
+        const celdaIndice = filasRestantes[i].querySelector("th");
+        if (celdaIndice) {
+          celdaIndice.textContent = i + 1;
+        }
+      }
+
+      Swal.fire({
+        title: "Contacto eliminado",
+        text: "El contacto fue eliminado satisfactoriamente",
+        icon: "success",
+      });
+      console.log(agenda);
+    }
+  });
+};
 
 window.prepararContacto = (id) => {
+
+  const contactoBuscado = agenda.find((contacto) => contacto.id === id);
+
+  inputNombre.value = contactoBuscado.nombre;
+  inputApellido.value = contactoBuscado.apellido;
+  inputEmail.value = contactoBuscado.email;
+  inputDireccion.value = contactoBuscado.direccion;
+  inputEmpresa.value = contactoBuscado.empresa;
+  inputImagen.value = contactoBuscado.imagen;
+  inputNotas.value = contactoBuscado.notas;
+  inputPuestoTrabajo.value = contactoBuscado.puestoTrabajo;
+  inputTelefono.value = contactoBuscado.telefono;
+  inputID = id;
+
+  estoyCreando = false;
+
+  modalFormularioContacto.show();
+  tituloModal.textContent = "Editar contacto"
+
+};
+
+
+const editarContacto = () => {
+
+  const indiceContacto = agenda.findIndex((contacto) => contacto.id === inputID);
+
+  agenda[indiceContacto].nombre = inputNombre.value;
+  agenda[indiceContacto].apellido = inputApellido.value;
+  agenda[indiceContacto].email = inputEmail.value;
+  agenda[indiceContacto].telefono = inputTelefono.value;
+  agenda[indiceContacto].imagen = inputImagen.value;
+  agenda[indiceContacto].empresa = inputEmpresa.value;
+  agenda[indiceContacto].puestoTrabajo = inputPuestoTrabajo.value;
+  agenda[indiceContacto].direccion = inputDireccion.value;
+  agenda[indiceContacto].notas = inputNotas.value;
+
+  //actualizando el localStorage
+  guardarLocalStorage();
+
+  //actualizado la fila de la tabla
+  const filaEditada = tbody.children[indiceContacto];
+  if (filaEditada) {
+    filaEditada.children[1].textContent = agenda[indiceContacto].nombre;
+    filaEditada.children[2].textContent = agenda[indiceContacto].apellido;
+    filaEditada.children[3].textContent = agenda[indiceContacto].telefono;
+    filaEditada.children[4].children[0].src = agenda[indiceContacto].nombre;
+  }
+
+  modalFormularioContacto.hide();
+
+  Swal.fire({
+        title: "Contacto actualizado",
+        text: `El contacto ${agenda[indiceContacto].nombre} fue editado satisfactoriamente`,
+        icon: "success",
+        confirmButtonText : "Ok",
+      });
 
 }
 
 window.verDetalleContacto = (id) => {
-
-}
+  const contactoBuscado = agenda.find((contacto) => contacto.id === id);
+  console.log(contactoBuscado);
+  
+};
 
 const mostrarNoHayDisponibles = () => {
   const parrafo = document.createElement("p");
-  parrafo.classList.add('text-center');
+  parrafo.classList.add("text-center");
   parrafo.textContent = "No hay contactos disponibles";
   tabla.appendChild(parrafo);
-}
+};
 
 const modalFormularioContacto = new bootstrap.Modal(
   document.getElementById("contactoModal")
@@ -187,8 +244,13 @@ btnAgregarContacto.addEventListener("click", () => {
 
 formularioContacto.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  crearContacto();
+  //aqui tengo que crear/editar
+  if (estoyCreando) {
+    crearContacto();
+  } else {
+    editarContacto();
+  }
+  
 });
 
 cargarContactos();
